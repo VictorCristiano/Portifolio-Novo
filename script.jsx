30e4e6f0e5c7b0847a24825c2a4b71fce3efe03b
Victor Cristiano <Victorcristianoods@gmail.com>
2023-03-03 08:59:06 -0300

    initial commit

---
+++ script.jsx
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["Estudante", "Apaixonado por Tecnologia", "Desenvolvedor Front-End", "Futuro Analista de Sistemas"],
+  typeSpeed: 100,
+  backSpeed: 70,
+  backDelay: 1000,
+  loop: true
+});

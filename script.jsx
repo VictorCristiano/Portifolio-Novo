7769bbf81137f364eee45ab64243e49f700e1db5
Victor Cristiano <Victorcristianoods@gmail.com>
2023-04-11 20:07:05 -0300

    realizada altaeracoes em responsivo

--- script.jsx
+++ script.jsx
@@ -5,3 +5,18 @@ var typed = new Typed(".multiple-text", {
   backDelay: 1000,
   loop: true
 });
+
+
+const inputName = document.getElementById("inputName");
+const inputComment = document.getElementById("inputComment");
+const form = document.getElementById("formulario");
+const postComments = document.getElementById("postComments");
+
+form.addEventListener('submit', (event)=>{
+  event.preventDefault()
+  let p = document.createElement('p');
+  p.innerHTML = `<strong>${inputName.value}: </strong> &nbsp ${inputComment.value}`;
+  postComments.appendChild(p);
+  inputName.value = '';
+  inputComment.value='';
+})
